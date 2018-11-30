dc1c5339454b255eaa1635ab1837b1b78883d944
Konstantin Bondar <konstantin.bondar@postindustria.com>
2018-11-30 21:54:48 +0200

    Init commit

---
+++ TermiteBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TermiteBook
+//
+//  Created by Konstantin Bondar on 30/11/2018.
+//  Copyright © 2018 SocialTermite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
